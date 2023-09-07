dd0ac80256091fcfb2a29d0cbaf261637965a60c
Daniel-Aldana10 <da341675@gmail.com>
2023-09-06 20:00:53 -0500

    Terceraentrega

--- Libreriacomplejos.py
+++ Libreriacomplejos.py
@@ -29,7 +29,7 @@ def complejo(A):
 
 def sumvectores(A,B):
     tam = int(len(A))
-    vect = np.ones(tam, dtype = tuple)
+    vect = [(0, 0) for j in range(tam)]
     for i in range(tam):
         vect[i] = sumacomplejos(A[i], B[i])
     return vect
@@ -43,14 +43,14 @@ def inversoaditivovec(A):
     return A
 def escalarmulvec(A,B):
     tam = int(len(B))
-    vect = np.ones(tam, dtype=tuple)
+    vect = [(0, 0) for j in range(tam)]
     for i in range(tam):
         vect[i] = multiplicacion(A,B[i])
     return vect
 def adicionmatrices(A,B):
     filas = len(A)
     columnas = len(A[0])
-    mat = np.zeros((filas, columnas), dtype= tuple)
+    mat = [[(0, 0) for k in range(columnas)] for j in range(filas)]
     for j in range(filas):
         for k in range(columnas):
             mat[j][k] = sumacomplejos(A[j][k],B[j][k])
@@ -65,22 +65,24 @@ def inversomatriz(A):
 def matrizporescalar(A,B):
     filas = len(B)
     col = len(B[0])
-    mat = np.ones((filas, col), dtype=tuple)
+    mat = [[(0, 0) for k in range(col)] for j in range(filas)]
     if len(A) > 1:
         for j in range(filas):
             for k in range(col):
                 mat[j][k] = multiplicacion(A, B[j][k])
         return mat
 def transpuesta(A):
-    fil = int(len(A))
-    col = int(len(A[0]))
-    mat = np.zeros((col,fil), dtype = tuple)
-    for j in range(col):
-        for k in range(fil):
-            mat[j][k] = A[k][j]
-    return mat
+    try:
+        fil = int(len(A))
+        col = int(len(A[0]))
+        mat = [[(0, 0) for k in range(fil)] for j in range(col)]
+        for j in range(col):
+            for k in range(fil):
+                mat[j][k] = A[k][j]
+        return mat
+    except:
+        return A
 def conjugadavecmat(A):
-
     try:
         fil = (len(A))
         col = len(A[0])
@@ -93,4 +95,65 @@ def conjugadavecmat(A):
         for j in range(fil):
             A[j] = conjudado(A[j])
         return A
+def daga(A):
+    try:
+        fil = (len(A))
+        col = len(A[0])
+        A = transpuesta(A)
+        for j in range(fil):
+            for k in range(col):
+                A[j][k] = conjudado(A[j][k])
+        return A
+    except:
+        fil = int(len(A))
+        for j in range(fil):
+            A[j] = conjudado(A[j])
+        return A
+def accionvecmat(A,B):
+    filA = len(A)
+    colA = len(A[0])
+    filB = len(B)
 
+    if colA == filB:
+        mat = [(0, 0) for j in range(filB)]
+
+        for j in range(filA):
+            for k in range(filB):
+                    mat[j] = sumacomplejos(mat[j], multiplicacion(A[j][k], B[k]))
+        return mat
+def multmatrices(A,B):
+    filA = len(A)
+    colA = len(A[0])
+    filB = len(B)
+    colB = len(B[0])
+    if colA == filB:
+        mat = [[(0,0) for k in range(colB)] for j in range(filA)]
+        for j in range(filA):
+            for k in range(colB):
+                for a in range(colA):
+                    mat[j][k] = sumacomplejos(mat[j][k], multiplicacion(A[j][a], B[a][k]))
+        return mat
+    else:
+        return "error"
+def producintern(A, B):
+
+    res = (0,0)
+    for i in range(len(A)):
+        res = sumacomplejos(res,multiplicacion(conjudado(A[i]),B[i]))
+
+    return res
+def norma(A):
+    res = (0,0)
+    for i in range(len(A)):
+        res = sumacomplejos(res,multiplicacion(conjudado(A[i]), A[i]))
+    return res[0]**(1/2)
+def distancia(A,B):
+    res = [(0,0) for j in range(len(A))]
+    for i in range(len(A)):
+        res[i] = resta(B[i],A[i])
+
+    return norma(res)
+def valorpropio(A,B):
+    resultado = accionvecmat(A,B)
+    escalar = resultado[1][0]/B[0][1]
+    return escalar
--- Test_vector_matriz.py
+++ Test_vector_matriz.py
@@ -6,37 +6,32 @@ class TestStringMethods(unittest.TestCase):
         vector1, vector2 = [(4,3),(1,2),(44,2)], [(1,4),(5,9),(4,3)]
         suma = lc.sumvectores(vector1,vector2)
         resultado =  [(5,7),(6,11),(48,5)]
-        for i in range(len(resultado)):
-            self.assertAlmostEqual(suma[i],resultado[i])
+        self.assertEqual(suma,resultado)
         vector1, vector2 = [(-4, 9), (5, 0), (3, 1)], [(5, 4), (3, 1), (2, 3)]
         suma = lc.sumvectores(vector1, vector2)
         resultado = [(1, 13), (8, 1), (5, 4)]
-        for i in range(len(resultado)):
-            self.assertAlmostEqual(suma[i], resultado[i])
+        self.assertEqual(suma, resultado)
     def test_inversovector(self):
         vector = [(-3,4),(5,4),(8,0)]
         inverso = lc.inversoaditivovec(vector)
         resultado = [(3,-4),(-5,-4),(-8,0)]
-        for i in range(len(resultado)):
-            self.assertAlmostEqual(inverso[i], resultado[i])
+        self.assertEqual(inverso, resultado)
         vector = [(5, 1), (0, 4), (3, 5)]
         inverso = lc.inversoaditivovec(vector)
         resultado = [(-5, -1), (0, -4), (-3, -5)]
-        for i in range(len(resultado)):
-            self.assertAlmostEqual(inverso[i], resultado[i])
+        self.assertEqual(inverso, resultado)
     def test_multvecesc(self):
         escalar = (2,3)
         vector = [(4,1),(1,0),(5,-3)]
         multplicacion = lc.escalarmulvec(escalar, vector)
         resultado = [(5,14),(2,3),(19,9)]
-        for i in range(len(resultado)):
-            self.assertAlmostEqual(multplicacion[i], resultado[i])
+
+        self.assertEqual(multplicacion, resultado)
         escalar = (2, 0)
         vector = [(4, 1), (1, 0), (5, -3)]
         multplicacion = lc.escalarmulvec(escalar, vector)
         resultado = [(8, 2), (2, 0), (10, -6)]
-        for i in range(len(resultado)):
-            self.assertAlmostEqual(multplicacion[i], resultado[i])
+        self.assertEqual(multplicacion, resultado)
     def test_sumamtrices(self):
         matriz1 = [[(3,1),(0,1)],
                    [(2,8),(3,2)]]
@@ -45,9 +40,7 @@ class TestStringMethods(unittest.TestCase):
         resultado = [[(13, 6), (4, 4)],
                     [(5, 7), (7, 8)]]
         suma = lc.adicionmatrices(matriz1,matriz2)
-        for j in range(len(matriz1)):
-            for k in range(len(matriz1[0])):
-                self.assertAlmostEqual(suma[j][k], resultado[j][k])
+        self.assertEqual(suma, resultado)
         matriz1 = [[(3, 1), (0, 1), (9, 2)],
                    [(2, 8), (3, 2), (4, 6)]]
         matriz2 = [[(10, 5), (4, 3), (0, -8)],
@@ -55,9 +48,7 @@ class TestStringMethods(unittest.TestCase):
         resultado = [[(13, 6), (4, 4), (9, -6)],
                      [(5, 7), (7, 8), (44, 9)]]
         suma = lc.adicionmatrices(matriz1, matriz2)
-        for j in range(len(matriz2)):
-            for k in range(len(matriz2[0])):
-                self.assertAlmostEqual(suma[j][k], resultado[j][k])
+        self.assertEqual(suma, resultado)
     def test_inversamatriz(self):
         matriz = [[(3,-5),(-5,4),(0,0)],
                   [(4,5),(1,-5),(4,6)],
@@ -66,17 +57,13 @@ class TestStringMethods(unittest.TestCase):
                   [(-4,-5),(-1,5),(-4,-6)],
                   [(-3,-1),(5,-4),(-4,-5)]]
         inversa = lc.inversomatriz(matriz)
-        for j in range(len(matriz)):
-            for k in range(len(matriz[0])):
-                self.assertAlmostEqual(inversa[j][k], resultado[j][k])
+        self.assertEqual(inversa, resultado)
         matriz = [[(10, 4), (3, 2)],
                   [(0, 1), (3, 2)]]
         resultado = [[(-10, -4), (-3, -2)],
                      [(0, -1), (-3, -2)]]
         inversa = lc.inversomatriz(matriz)
-        for j in range(len(matriz)):
-            for k in range(len(matriz[0])):
-                self.assertAlmostEqual(inversa[j][k], resultado[j][k])
+        self.assertEqual(inversa, resultado)
     def test_multescmatriz(self):
         escalar = (2,3)
         matriz = [[(4,1),(1,0)],
@@ -84,18 +71,14 @@ class TestStringMethods(unittest.TestCase):
         resultado = [[(5,14),(2,3)],
                      [(8,12),(-3,2)]]
         multiplicacion = lc.matrizporescalar(escalar,matriz)
-        for j in range(len(matriz)):
-            for k in range(len(matriz[0])):
-                self.assertAlmostEqual(multiplicacion[j][k], resultado[j][k])
+        self.assertEqual(multiplicacion, resultado)
         escalar = (2, 0)
         matriz = [[(4, 1), (1, 0)],
                   [(4, 0), (0, 1)]]
         resultado = [[(8, 2), (2, 0)],
                      [(8, 0), (0, 2)]]
         multiplicacion = lc.matrizporescalar(escalar, matriz)
-        for j in range(len(matriz)):
-            for k in range(len(matriz[0])):
-                self.assertAlmostEqual(multiplicacion[j][k], resultado[j][k])
+        self.assertEqual(multiplicacion, resultado)
     def test_transpuesta(self):
         matriz = [[(1,0),(4,5),(8,4)],
                   [(-5,4),(-4,3),(4,2)]]
@@ -103,30 +86,104 @@ class TestStringMethods(unittest.TestCase):
                      [(4,5),(-4,3)],
                      [(8,4),(4,2)]]
         transpuesta = lc.transpuesta(matriz)
-        for j in range(len(transpuesta)):
-            for k in range(len(transpuesta[0])):
-                self.assertAlmostEqual(transpuesta[j][k], resultado[j][k])
+        self.assertEqual(transpuesta, resultado)
         matriz = [[(1,0),(4,5)],
                   [(-5,4),(-4,3)]]
         resultado = [[(1,0),(-5,4)],
                      [(4,5),(-4,3)]]
         transpuesta = lc.transpuesta(matriz)
-        for j in range(len(transpuesta)):
-            for k in range(len(transpuesta[0])):
-                self.assertAlmostEqual(transpuesta[j][k], resultado[j][k])
+        self.assertEqual(transpuesta, resultado)
+
     def test_conjugada(self):
         vector = [(1, -2), (2, 4), (7, 8)]
         resultado = [(1,2),(2,-4),(7,-8)]
         conjugado = lc.conjugadavecmat(vector)
-        for j in range(len(conjugado)):
-            self.assertAlmostEqual(conjugado,resultado)
+        self.assertEqual(conjugado,resultado)
         matriz = [[(4,3),(4,8)],
                   [(54,11),(4,96)]]
         resultado = [[(4,-3),(4,-8)],
                      [(54,-11),(4,-96)]]
         conjugado = lc.conjugadavecmat(matriz)
-        for j in range(len(matriz)):
-            for k in range(len(matriz[0])):
-                self.assertAlmostEqual(conjugado[j][k], resultado[j][k])
+        self.assertEqual(conjugado, resultado)
+    def test_adjunta(self):
+        vector = [(1, -2), (2, 4), (7, 8)]
+        resultado = [(1, 2), (2, -4), (7, -8)]
+        adjunta = lc.conjugadavecmat(vector)
+        self.assertEqual(adjunta, resultado)
+        matriz = [[(4, 3), (4, 8)],
+                [(54, 11), (4, 96)]]
+        resultado = [[(4, -3), (54, -11)],
+                     [(4, -8), (4, -96)]]
 
-
+        adjunta = lc.daga(matriz)
+        self.assertEqual(adjunta, resultado)
+    def test_productomatrices(self):
+        matriz = [[(4, 1), (1, 0)],
+                  [(4, 0), (0, 1)]]
+        matriz2 = [[(4, 1), (1, 0)],
+                  [(4, 0), (0, 1)]]
+        resultado = [[(19,8),(4,2)],[(16,8),(3,0)]]
+        mult = lc.multmatrices(matriz,matriz2)
+        self.assertEqual(mult, resultado)
+        matriz = [[(1, 1), (1, 5)],
+                  [(5, 9), (4, 1)]]
+        matriz2 = [[(4, 1), (1, 0)],
+                   [(4, 0), (0, 1)]]
+        resultado = [[(7,25),(-4,2)],[(27,45),(4,13)]]
+        mult = lc.multmatrices(matriz, matriz2)
+        self.assertEqual(mult, resultado)
+    def test_accionvector(self):
+        matriz = [[(1, 1), (1, 5)],
+                  [(5, 9), (4, 1)]]
+        vector = [(5,2),(3,0)]
+        resultado = [(6,22),(19,58)]
+        accion = lc.accionvecmat(matriz,vector)
+        self.assertEqual(accion, resultado)
+        matriz = [[(4, 1), (1, 0)],
+                   [(4, 0), (0, 1)]]
+        vector = [(5, 8), (4, 20)]
+        resultado = [(16, 57), (0, 36)]
+        accion = lc.accionvecmat(matriz, vector)
+        self.assertEqual(accion, resultado)
+    def test_productointerno(self):
+        v1 = [(1,0),(2,3),(0,6)]
+        v2 = [(0,0),(0,1),(2,4)]
+        resultado = (27,-10)
+        producto = lc.producintern(v1,v2)
+        self.assertEqual(producto, resultado)
+        v1 = [(1, 0), (0, 1), (1, -3)]
+        v2 = [(2, 1), (0, 1), (2, 0)]
+        resultado = (5, 7)
+        producto = lc.producintern(v1, v2)
+        self.assertEqual(producto, resultado)
+    def test_norma(self):
+        v1 = [(4,3),(6,-4),(12,-7),(0,13)]
+        resultado = 439**(1/2)
+        norma = lc.norma(v1)
+        self.assertEqual(norma, resultado)
+        v1 = [(2,5),(1,-4),(0,-7),(0,3)]
+        resultado = 104 ** (1 / 2)
+        norma = lc.norma(v1)
+        self.assertEqual(norma, resultado)
+    def test_distancia(self):
+        v1 = [(0,2),(3,0),(0,4)]
+        v2 = [(0,1),(-3,0),(0,-5)]
+        resultado = 118**(1/2)
+        distancia = lc.distancia(v1,v2)
+        self.assertEqual(resultado,distancia)
+        v1 = [(3,2),(4,0)]
+        v2 = [(4,0),(3,2)]
+        resultado = 10** (1 / 2)
+        distancia = lc.distancia(v1, v2)
+        self.assertEqual(resultado, distancia)
+    def test_valorespropios(self):
+       matriz = [[(0,0),(0,4)],[(0,-4),(0,0)]]
+       vector = [(0,1),(1,0)]
+       resultado = 4.0
+       valor = lc.valorpropio(matriz,vector)
+       self.assertEqual(resultado,valor)
+       matriz = [[(0, 0), (0, 41)], [(0, -41), (0, 0)]]
+       vector = [(0, 4), (4, 0)]
+       resultado = 41.0
+       valor = lc.valorpropio(matriz, vector)
+       self.assertEqual(resultado, valor)
